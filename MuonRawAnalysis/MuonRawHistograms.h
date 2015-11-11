7d2612d19eb2d2d583f53941adf9a1151c44ddcb
alexandertuna <alexander.tuna@gmail.com>
2015-11-11 08:56:09 -0500

    sadness

--- MuonRawAnalysis/MuonRawHistograms.h
+++ MuonRawAnalysis/MuonRawHistograms.h
@@ -1,6 +1,17 @@
 #ifndef MUONRAWHITS_MUONRAWHISTOGRAMS_H
 #define MUONRAWHITS_MUONRAWHISTOGRAMS_H
 
+#include <vector>
+#include <string>
+
+#include <TFile.h>
+#include <TTree.h>
+#include <TTreeReader.h>
+#include <TTreeReaderValue.h>
+#include <TTreeReaderArray.h>
+
+#include <TH1.h>
+
 class MuonRawHistograms {
 
  public:
@@ -11,6 +22,46 @@ class MuonRawHistograms {
     int initialize();
     int execute();
     int finalize();
+
+    TFile *file;
+    TTreeReader treereader;
+    TTree *tree;
+
+    int RunNumber; 
+    int EventNumber;
+    int lbn;
+    int bcid;
+    int colliding_bunches;
+    double avgIntPerXing;
+    double actIntPerXing;
+    double lbAverageLuminosity;
+    double lbLuminosityPerBCID;
+    double prescale_L1;
+    double prescale_HLT;
+
+    int mdt_chamber_n;
+    std::vector<std::string>* mdt_chamber_type; //!
+    std::vector<std::string>* mdt_chamber_side; //!
+    std::vector<int>*         mdt_chamber_eta_station; //!
+    std::vector<int>*         mdt_chamber_phi_sector; //!
+
+    std::vector<int>*              mdt_chamber_tube_n; //!
+    std::vector<std::vector<int>>* mdt_chamber_tube_r; //!
+    std::vector<std::vector<int>>* mdt_chamber_tube_adc; //!
+
+    int csc_chamber_n;
+    std::vector<int>*         csc_chamber_r; //!
+    std::vector<std::string>* csc_chamber_type; //!
+    std::vector<std::string>* csc_chamber_side; //!
+    std::vector<int>*         csc_chamber_phi_sector; //!
+
+    std::vector<int>*              csc_chamber_cluster_n; //!
+    std::vector<std::vector<int>>* csc_chamber_cluster_r; //!
+    std::vector<std::vector<int>>* csc_chamber_cluster_rmax; //!
+    std::vector<std::vector<int>>* csc_chamber_cluster_qsum; //!
+    std::vector<std::vector<int>>* csc_chamber_cluster_qmax; //!
+    std::vector<std::vector<int>>* csc_chamber_cluster_strips; //!
+    std::vector<std::vector<int>>* csc_chamber_cluster_measuresphi; //!
 
 };
 
--- Root/MuonRawHistograms.cxx
+++ Root/MuonRawHistograms.cxx
@@ -1,6 +1,16 @@
 #include "MuonRawAnalysis/MuonRawHistograms.h"
 
 #include <iostream>
+#include <vector>
+#include <string>
+
+#include <TFile.h>
+#include <TTree.h>
+#include <TTreeReader.h>
+#include <TTreeReaderValue.h>
+#include <TTreeReaderArray.h>
+
+#include <TH1.h>
 
 MuonRawHistograms::MuonRawHistograms(){
     
@@ -10,4 +20,85 @@ MuonRawHistograms::MuonRawHistograms(){
 }
 
 MuonRawHistograms::~MuonRawHistograms(){}
+
+int MuonRawHistograms::initialize(){
+
+    file = TFile::Open("/n/atlasfs/atlasdata/tuna/MuonRawHits/ntuples-2015-11-08-09h00m45s/00281143/ntuple_0000.root");
+    if (!file){
+        std::cout << "FATAL MuonRawHistograms::initialize: no file" << std::endl;
+        return 1;
+    }
+    else{
+        std::cout << "file " << std::endl;
+    }
+
+    tree = (TTree*)(file->Get("physics"));
+    if (!tree){
+        std::cout << "FATAL MuonRawHistograms::initialize: no tree" << std::endl;
+        return 1;
+    }
+    else{
+        std::cout << "tree " << tree->GetEntries() << std::endl;
+    }
+    
+    tree->SetBranchAddress("RunNumber",           &RunNumber);
+    tree->SetBranchAddress("EventNumber",         &EventNumber);
+    tree->SetBranchAddress("lbn",                 &lbn);
+    tree->SetBranchAddress("bcid",                &bcid);
+    tree->SetBranchAddress("colliding_bunches",   &colliding_bunches);
+    tree->SetBranchAddress("avgIntPerXing",       &avgIntPerXing);
+    tree->SetBranchAddress("actIntPerXing",       &actIntPerXing);
+    tree->SetBranchAddress("lbAverageLuminosity", &lbAverageLuminosity);
+    tree->SetBranchAddress("lbLuminosityPerBCID", &lbLuminosityPerBCID);
+    tree->SetBranchAddress("prescale_L1",         &prescale_L1);
+    tree->SetBranchAddress("prescale_HLT",        &prescale_HLT);
+
+    tree->SetBranchAddress("mdt_chamber_n",           &mdt_chamber_n);
+    // tree->SetBranchAddress("mdt_chamber_type",        &mdt_chamber_type);
+    // tree->SetBranchAddress("mdt_chamber_side",        &mdt_chamber_side);
+    // tree->SetBranchAddress("mdt_chamber_eta_station", &mdt_chamber_eta_station);
+    // tree->SetBranchAddress("mdt_chamber_phi_sector",  &mdt_chamber_phi_sector);
+
+    tree->SetBranchAddress("mdt_chamber_tube_n".c_str(),      &mdt_chamber_tube_n);
+    // tree->SetBranchAddress("mdt_chamber_tube_r",      &mdt_chamber_tube_r);
+    // tree->SetBranchAddress("mdt_chamber_tube_adc",    &mdt_chamber_tube_adc);
+
+    // tree->SetBranchAddress("csc_chamber_n",              &csc_chamber_n);
+    // tree->SetBranchAddress("csc_chamber_r",              &csc_chamber_r);
+    // tree->SetBranchAddress("csc_chamber_type",           &csc_chamber_type);
+    // tree->SetBranchAddress("csc_chamber_side",           &csc_chamber_side);
+    // tree->SetBranchAddress("csc_chamber_phi_sector",     &csc_chamber_phi_sector);
+
+    // tree->SetBranchAddress("csc_chamber_cluster_n",      &csc_chamber_cluster_n);
+    // tree->SetBranchAddress("csc_chamber_cluster_r",      &csc_chamber_cluster_r);
+    // tree->SetBranchAddress("csc_chamber_cluster_rmax",   &csc_chamber_cluster_rmax);
+    // tree->SetBranchAddress("csc_chamber_cluster_qsum",   &csc_chamber_cluster_qsum);
+    // tree->SetBranchAddress("csc_chamber_cluster_qmax",   &csc_chamber_cluster_qmax);
+    // tree->SetBranchAddress("csc_chamber_cluster_strips", &csc_chamber_cluster_strips);
+
+    std::cout << "fuck1 " << tree->GetEntries() << std::endl;
+    
+    return 0;
+}
+
+int MuonRawHistograms::execute(){
+
+    std::cout << "fuck2 " << tree->GetEntries() << std::endl;
+
+    int entries = 100; // (int)(tree->GetEntries());
+
+    for (int ent = 1; ent < entries; ++ent){
+
+        tree->GetEntry(ent);
+        std::cout << mdt_chamber_n << std::endl;
+
+    }
+
+    return 0;
+}
+
+int MuonRawHistograms::finalize(){
+
+    return 0;
+}
 
