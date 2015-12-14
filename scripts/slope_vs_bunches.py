6d3e61d1850c6251b22f1ec9de83285c348f8864
alexandertuna <alexander.tuna@gmail.com>
2015-12-13 21:26:59 -0500

    robustifying, make 7e34 the default hllhc

--- scripts/slope_vs_bunches.py
+++ scripts/slope_vs_bunches.py
@@ -14,7 +14,7 @@ bunches_run3  = 2808
 bunches_hllhc = 3564
 
 lumi_run3  = 10
-lumi_hllhc = 50
+lumi_hllhc = 70
 
 runs = [
     "00278880",
@@ -48,6 +48,7 @@ graph["EIL1"] = ROOT.TGraph(len(bunches), array.array("d", bunches), array.array
 graph["EIS1"] = ROOT.TGraph(len(bunches), array.array("d", bunches), array.array("d", slope["EIS1"]))
 
 expression = "[0] + [1]/x"
+# expression = "[0] + [1]/x + [2]/(x*x)"
 for region in graph:
     fit[region] = ROOT.TF1("fit_"+region, expression, 0, 3800)
     graph[region].Fit(fit[region], "RWQN")
@@ -96,7 +97,8 @@ for line in [line_run3, line_hllhc]:
     line.Draw()
 
 print
-print " %7s %7s %10s | %10s %10s | %10s %10s" % ("region", "[0]", "[1]", "slope@2808", "slope@3564", "kHz@2808", "kHz@3564")
+print " %7s %7s +/- %7s | %10s +/- %10s | %10s %10s | %10s %10s" % ("region", "[0]", " ", "[1]", " ", 
+                                                                    "slope@2808", "slope@3564", "kHz@2808", "kHz@3564")
 
 for region in graph:
     fit[region].SetLineColor(color(region))
@@ -107,14 +109,19 @@ for region in graph:
     slope_run3  = fit[region].GetParameter(0) + fit[region].GetParameter(1)/bunches_run3
     slope_hllhc = fit[region].GetParameter(0) + fit[region].GetParameter(1)/bunches_hllhc
 
-    print " %7s %7.3f %10.1f | %10.3f %10.3f | %10.1f %10.1f" % (region,
-                                                                 fit[region].GetParameter(0),
-                                                                 fit[region].GetParameter(1),
-                                                                 slope_run3,
-                                                                 slope_hllhc,
-                                                                 lumi_run3  * slope_run3  / 1000,
-                                                                 lumi_hllhc * slope_hllhc / 1000,
-                                                                 )
+    slope_run3_up  = fit[region].GetParameter(0) + fit[region].GetParError(0) + (fit[region].GetParameter(1) + fit[region].GetParError(1))/bunches_run3
+    slope_hllhc_up = fit[region].GetParameter(0) + fit[region].GetParError(0) + (fit[region].GetParameter(1) + fit[region].GetParError(1))/bunches_hllhc
+
+    print " %7s %7.3f +/- %7.3f | %10.1f +/- %10.1f | %10.3f %10.3f | %10.3f %10.3f" % (region,
+                                                                                fit[region].GetParameter(0),
+                                                                                fit[region].GetParError(0),
+                                                                                fit[region].GetParameter(1),
+                                                                                fit[region].GetParError(1),
+                                                                                slope_run3,
+                                                                                slope_hllhc,
+                                                                                lumi_run3  * slope_run3  / 1000,
+                                                                                lumi_hllhc * slope_hllhc / 1000,
+                                                                                )
 print
 
 xcoord,ycoord = 0.7, 0.85
