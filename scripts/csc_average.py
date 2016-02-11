c1bc2aaaf63f2b88e5f349c1806a2ad15d7a3fe4
alexandertuna <alexander.tuna@gmail.com>
2016-02-11 14:31:28 +0100

    add efficiency

--- scripts/csc_average.py
+++ scripts/csc_average.py
@@ -1,11 +1,14 @@
 import ROOT
 
+hits       = "adc"
+efficiency = 1.0 if hits=="raw" else 0.789
+
 area  = ROOT.TFile.Open("area.root")
 hists = ROOT.TFile.Open("histograms.root")
 
 evts     = hists.Get("00284285/evts_00284285").GetBinContent(1)
-counts_L = hists.Get("00284285/hits_adc_vs_region_L_00284285")
-counts_S = hists.Get("00284285/hits_adc_vs_region_S_00284285")
+counts_L = hists.Get("00284285/hits_%s_vs_region_L_00284285" % (hits))
+counts_S = hists.Get("00284285/hits_%s_vs_region_S_00284285" % (hits))
 
 area_L = area.Get("area_vs_region_L")
 area_S = area.Get("area_vs_region_S")
@@ -31,8 +34,8 @@ rate_L_C = counts_L_C / (evts * livetime * area_L_C)
 rate_S_A = counts_S_A / (evts * livetime * area_S_A)
 rate_S_C = counts_S_C / (evts * livetime * area_S_C)
 
-rate_L = (counts_L_A+counts_L_C) / (evts * livetime * (area_L_A+area_L_C))
-rate_S = (counts_S_A+counts_S_C) / (evts * livetime * (area_S_A+area_S_C))
+rate_L = (counts_L_A+counts_L_C) / (evts * livetime * (area_L_A+area_L_C) * efficiency)
+rate_S = (counts_S_A+counts_S_C) / (evts * livetime * (area_S_A+area_S_C) * efficiency)
 
 print "L  A: %.1f | C: %.1f | avg: %.1f" % (rate_L_A, rate_L_C, rate_L)
 print "S  A: %.1f | C: %.1f | avg: %.1f" % (rate_S_A, rate_S_C, rate_S)
